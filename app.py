c1665fff83fa6cdd06608cd3d460a5e0a8d77bff
whit33y <troppout@gmail.com>
2022-05-02 16:48:30 +0200

    Created result.html, function to change currency

--- app.py
+++ app.py
@@ -5,13 +5,25 @@ c = CurrencyRates()
 
 app=Flask(__name__)
 
+def change_currency(currency_a, currency_b, how_much_money):
+    a = c.get_rate(currency_a, currency_b) * how_much_money
+    return a
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    zlotowka = round(c.get_rate('USD', 'PLN'),2)
-    return render_template('index.html', zlotowka=zlotowka)
+    if request.method == 'POST':
+        first_currency = request.form.get('first_currency')
+        second_currency = request.form.get('second_currency')
+        money = float(request.form.get('money'))
+        zlotowka = round(change_currency(first_currency, second_currency, money),2)
+        return render_template('index.html', money=money, first_currency=first_currency, second_currency=second_currency, zlotowka=zlotowka)
+
+    return render_template('index.html')
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('error.html')
 if __name__=="__main__":
     app.run(debug=True)
+
+
