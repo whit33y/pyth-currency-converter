571555df13ac6f43ef6b2f8e254764c3c616cac0
whit33y <troppout@gmail.com>
2022-05-02 16:04:39 +0200

    Added requirements and basic form in index.html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, redirect, render_template, request
+import requests
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
+
 app=Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    return render_template('index.html')
+    zlotowka = round(c.get_rate('USD', 'PLN'),2)
+    return render_template('index.html', zlotowka=zlotowka)
 
 @app.errorhandler(404)
 def not_found(error):
